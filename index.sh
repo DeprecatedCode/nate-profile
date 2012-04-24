2c134798786517661c13cecf7f2c2dbbcce75800
Nate Ferrero <nateferrero@gmail.com>
2012-04-24 23:26:13 +0000

    Basic commands

---
+++ index.sh
@@ -0,0 +1,17 @@
+# Nate Ferrero
+# nateferrero.com
+
+# This Document
+alias pr='nano ~/nate-profile/index.sh'
+alias ra='source ~/nate-profile/index.sh'
+
+# Nginx
+alias nns='sudo /etc/init.d/nginx start'
+alias nnp='sudo /etc/init.d/nginx stop'
+alias nnr='sudo /etc/init.d/nginx reload'
+
+# Git
+alias pull='git pull'
+alias push='git push'
+alias sync='git pull && git push' 
+alias gs='git status'
