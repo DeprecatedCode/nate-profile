85be8baf616dc4f3f92a2684cb0d9ae69e2d2a7c
Nate Ferrero <nateferrero@gmail.com>
2012-04-26 05:44:51 +0000

    Added iterative git status hp and hup

--- index.sh
+++ index.sh
@@ -16,6 +16,10 @@ alias push='git push'
 alias sync='git pull && git push' 
 alias gs='git status'
 
+# Check All
+alias hup='for d in `find ~/*/.git -maxdepth 0 -type d` ; do echo "\n\033[1;35m$d\\033[0m"; cd "$d/.."; git pull ; git status ; done ; cd ~/ ; echo "\n"'
+alias hp='for d in `find ~/*/.git -maxdepth 0 -type d` ; do echo "\n\033[1;35m$d\\033[0m"; cd "$d/.."; git status ; done ; cd ~/ ; echo "\n"'
+
 # Custom - VPS Status
 alias vps='nodemon --no-stdin ~/nate-vps-system/app.js &'
 alias guide='nodemon --no-stdin ~/snapplab-guide/app.js &'
